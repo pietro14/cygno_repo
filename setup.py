d4c2a30f48e9767e5d0f5fef4df0bdb9a133a1a9
Giovanni Mazzitelli <45018579+gmazzitelli@users.noreply.github.com>
2021-10-19 21:02:53 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cygno_repo',                                            # package name
+    author="Giovanni Mazzitelli",
+    author_email="giovanni.mazzitelli@lnf.infn.it",
+    version='0.1',                          # version
+    description='Packge to hadle Cygno experimet repository',      # short description
+    url='https://github.com/gmazzitelli/cygno_repo',               # package URL
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+
+)
