23dd72f8973b8d757aeaec39e60db36f8a812b96
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2021-11-05 08:24:48 +0100

    v0.1.2 package, his2root

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         "Development Status :: 3 - Alpha",
         "Operating System :: OS Independent",
     ],
-    scripts=['bin/cygno_repo', 'bin/cygno_runs'],
+    scripts=['bin/cygno_repo', 'bin/cygno_runs', 'bin/his2root'],
     python_requires='>=3.5',
     install_requires=[
     'boto3',
